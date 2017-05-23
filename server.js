32ab82cf7f6df5949fc841e6f964ecf890c7ae29
messagetonishu <messagetonishu@gmail.com>
2017-05-22 20:19:58 -0500

    iniital commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send({name:"Jagadeesh"})
+})
+
+app.get('/welcome', function (req, res) {
+  res.send("Welcome to nodeJS");
+})
+
+app.get('/about', function(req,req){
+	res.send("this is Project Base Traing");
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
